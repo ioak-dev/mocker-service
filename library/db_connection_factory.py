05d42233a63f066936c552bfbf4a4d260a9ec674
Arun Kumar Selvaraj <dev@ioak.org>
2021-01-20 15:10:15 +0530

    Deployment changes

--- library/db_connection_factory.py
+++ library/db_connection_factory.py
@@ -1,13 +1,20 @@
 from pymongo import MongoClient
 import os
 
-DATABASE_URI = os.environ.get('DATABASE_URI')
-if DATABASE_URI is None:
-    DATABASE_URI = 'mongodb://localhost:27017'
+MONGODB_URI = os.environ.get('MONGODB_URI')
+if MONGODB_URI is None:
+    MONGODB_URI = 'mongodb://localhost:27017'
 
 __connection_map = {}
 
-def get_collection(space, collection):
-    if space not in __connection_map.keys():
-        __connection_map[space] = MongoClient(DATABASE_URI)[space]
-    return __connection_map.get(space)[collection]
+def get_collection(space_id, collection):
+    if space_id not in __connection_map.keys():
+        __connection_map[space_id] = MongoClient(MONGODB_URI)[space_id]
+    return __connection_map.get(space_id)[collection]
+
+def drop_database(space_id):
+    MongoClient(MONGODB_URI).drop_database(space_id)
+
+def get_client():
+    return MongoClient(MONGODB_URI)
+
